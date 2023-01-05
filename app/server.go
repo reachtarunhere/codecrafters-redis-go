462269f18a63719d5f8bcb30eb9560d825d8d7f9
Tarun Vangani <reachtarunhere@gmail.com>
2023-01-05 17:30:49 +0800

    fixed response for PING after accepting connection

--- app/server.go
+++ app/server.go
@@ -18,9 +18,16 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+        conn , err := l.Accept()
+	fmt.Println("Accepted connection")
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	_, err = conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
+		os.Exit(1)
+	}
+	
 }
